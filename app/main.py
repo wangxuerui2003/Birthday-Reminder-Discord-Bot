2a50886a3ab657c70ae77d6b9f36a133cb134f20
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-04-27 12:41:11 +0800

    Save my work

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
-from discord.ui import Select, View
+from discord.interactions import Interaction
+from discord.ui import Modal, TextInput
 import datetime
 import dotenv
 from pathlib import Path
@@ -12,72 +13,44 @@ os.environ["SSL_CERT_FILE"] = certifi.where()
 
 dotenv.load_dotenv(Path('../.env'))
 
-class BirthdayView(View):
-    def __init__(self):
-        super().__init__()
-        self.add_item(Select(custom_id='birthmonth', options=[
-            discord.SelectOption(label='January', value='01'),
-            discord.SelectOption(label='February', value='02'),
-            discord.SelectOption(label='March', value='03'),
-            discord.SelectOption(label='April', value='04'),
-            discord.SelectOption(label='May', value='05'),
-            discord.SelectOption(label='June', value='06'),
-            discord.SelectOption(label='July', value='07'),
-            discord.SelectOption(label='August', value='08'),
-            discord.SelectOption(label='September', value='09'),
-            discord.SelectOption(label='October', value='10'),
-            discord.SelectOption(label='November', value='11'),
-            discord.SelectOption(label='December', value='12')
-        ]))
-        self.add_item(discord.ui.TextInput(label="birthday", required=True, placeholder="Day", max_length=2))
-        self.add_item(discord.ui.TextInput(label="birthyear", required=True, placeholder="Year", max_length=2))
-
-    async def interaction_check(self, interaction: discord.Interaction):
-        if interaction.user.id != self.message.author.id:
-            await interaction.response.send_message('You cannot interact with this message.', ephemeral=True)
-            return False
-        return True
-
-    @property
-    def birthday(self):
-        month = self.get_item(0).values[0]
-        day = self.get_item(1).values[0]
-        year = self.get_item(2).values[0]
-        return f"{month}/{day}/{year}"
-    
-class Cog(commands.Cog):
-	def __init__(self, bot):
-		super().__init__()
-		self.bot = bot
-    
-	@commands.command(name="set_birthday")
-	async def set_birthday(self, ctx):
-		view = BirthdayView()
-		message = await ctx.send('Please select your birthday:', view=view)
-		# interaction = await self.bot.wait_for('select_option', check=view.interaction_check)
-		# await interaction.response.defer()
-		# await message.edit(content=f"You selected {view.birthday} as your birthday.")
-
-		# # Check if the birthday is valid
-		# try:
-		# 	birthday = datetime.datetime.strptime(view.birthday, '%m/%d/%Y')
-		# 	await ctx.send('Your birthday is valid!')
-		# except ValueError:
-		# 	await ctx.send('Please enter a valid date in the format MM/DD/YYYY.')
-		await ctx.send("Haha")
-    
-class Bot(commands.Bot):
-	async def on_ready(self):
-		print(f'Logged in as {self.user}.')
-
-	async def setup(self):
-		await self.add_cog(Cog(self))
-
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-bot = Bot(command_prefix='!', intents=intents)
-asyncio.run(bot.setup())
-# tree = discord.app_commands.CommandTree(client)
+
+class BirthdayModal(Modal, title="Birthday Reminder"):
+    answer = TextInput(
+        label="Enter your birthday (dd/mm/yyyy) | (Enter 0000 for anonymous birthyear)",
+        style=discord.TextStyle.short,
+        placeholder="21/11/2003",
+        required=True,
+        max_length=10,
+        min_length=10
+    )
+
+    async def on_submit(self, interaction: Interaction[ClientT]) -> Coroutine[Any, Any, None]:
+        return await super().on_submit(interaction)
+
+
+@bot.command()
+async def set_birthday(ctx):
+    def check(message):
+        try:
+            birthday = datetime.datetime.strptime(message.content, '%m/%d/%Y')
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    # text_input = TextInput(label="birthday")
+    # await ctx.send('Plase enter some text:')
+    # input_msg = await ctx.send(embed=discord.Embed(title="Enter some text:"))
+    # try:
+    #     input_text = await bot.wait_for('message', check=check, timeout=30)
+    #     await input_msg.delete()
+    #     await input_text.reply(f'You entered: {input_text.content}')
+    # except asyncio.TimeoutError:
+    #     await input_msg.delete()
+    #     await ctx.send('You took too long to enter text.')
 
 bot.run(os.getenv('TOKEN'))
