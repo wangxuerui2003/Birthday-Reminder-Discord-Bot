282c7cade4407ab54f43ef830f2a877cd8da6e78
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-05-04 11:12:50 +0800

    Change datatype of servere and channel id to varchar(255)

--- app/db_queries/create_birthdays_table.sql
+++ app/db_queries/create_birthdays_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE Birthdays (
 	user_id VARCHAR(255) NOT NULL,
 	username VARCHAR(255) NOT NULL,
 	birthday DATE NOT NULL,
-	server_id INT NOT NULL,
+	server_id VARCHAR(255) NOT NULL,
 	PRIMARY KEY(user_id),
 	FOREIGN KEY(server_id) REFERENCES Servers(server_id)
 );
--- app/db_queries/create_servers_table.sql
+++ app/db_queries/create_servers_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Servers (
-	server_id INT NOT NULL,
-	channel_id INT NOT NULL,
+	server_id VARCHAR(255) NOT NULL,
+	channel_id VARCHAR(255) NOT NULL,
 	PRIMARY KEY(server_id)
 );
